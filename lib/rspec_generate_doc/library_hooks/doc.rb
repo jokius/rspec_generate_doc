5fb5e818cfc3972cfa87b129b4551b3d94f5efaa
Игорь Кутявин <djok69@gmail.com>
2017-05-09 03:28:59 +0300

    Rename file and refactroring

--- lib/rspec_generate_doc/library_hooks/doc.rb
+++ lib/rspec_generate_doc/library_hooks/doc.rb
@@ -1,31 +1,37 @@
 require 'rspec/rails'
-require 'rspec_generate_doc/generate'
+require 'rspec_generate_doc/generate_file'
+require 'rspec_generate_doc/decorators/action'
 
 module RspecGenerateDoc
   module LibraryHooks
     module Doc
       ::RSpec.configure do |config|
         config.before(:context) do
-          @methods_hash = {}
+          @actions = []
           @is_correct_type = self.class.metadata[:type] == :controller
         end
 
         config.after(:each) do
           next unless @is_correct_type
-          key = self.class.description
+          name = self.class.description
           parent = self.class.parent
           loop do
             break if parent.nil? || parent.description == self.class.top_level_description
-            key = "#{parent.description} #{key}"
+            name = "#{parent.description} #{name}"
             parent = parent.parent
           end
 
-          skip = try(:skip_this) || @skip_this
-          @hash[key] ||= { response: response, api_params: (try(:api_params) || @api_params || {}) } unless skip
+          next if try(:skip_this) || @skip_this
+          api_params = try(:api_params) || @api_params || {}
+          opntions = try(:api_opntions) || @api_opntions || {}
+          @actions << RspecGenerateDoc.configuration.action_decorator
+                                      .new(name: name, response: response, api_params: api_params, options: opntions)
         end
 
         config.after(:context) do
-          RspecGenerateDoc::Genarate.new(self.class.top_level_description, @hash)
+          next unless @is_correct_type
+          parent = self.class.top_level_description
+          RspecGenerateDoc::GenarateFIle.new(parent: parent, actions: @actions).create_file_by_template
         end
       end
     end
