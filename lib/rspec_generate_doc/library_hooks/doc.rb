fda1efb18997348250b54840dc902051b74f025b
Jokius <djok69@gmail.com>
2017-10-05 04:44:25 +0300

    Refactoring

--- lib/rspec_generate_doc/library_hooks/doc.rb
+++ lib/rspec_generate_doc/library_hooks/doc.rb
@@ -26,14 +26,22 @@ module RspecGenerateDoc
           api_params = try(:api_params) || @api_params || {}
           options = try(:api_options) || @api_options || {}
           new_action = RspecGenerateDoc.configuration.action_decorator
-                                      .new(name: name, response: response, api_params: api_params, options: options)
-          @actions << new_action unless @actions.index { |action| action.name == new_action.name }
+                                       .new(name: name,
+                                            response: response,
+                                            api_params: api_params,
+                                            options: options)
+          unless @actions.index { |action| action.name == new_action.name }
+            @actions << new_action
+          end
         end
 
         config.after(:context) do
           next if @is_incorrect_type
-          parent = self.class.metadata[:api_name] || self.class.top_level_description
-          RspecGenerateDoc::GenarateFIle.new(parent: parent.to_s, actions: @actions).create_file_by_template
+          parent = self.class.metadata[:api_name] || self.class
+                                                         .top_level_description
+          RspecGenerateDoc::GenarateFIle
+            .new(parent: parent.to_s, actions: @actions)
+            .create_file_by_template
         end
       end
     end
